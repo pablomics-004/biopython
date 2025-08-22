9c02e746b1f94325c31d449c06b9296d893882a1
Pablo Salazar Méndez <pablosm@lcg.unam.mx>
2025-08-21 21:57:12 -0600

    Clase ncRNA

--- src/SalazarMendez_Pablo_Ejercicio3.py
+++ src/SalazarMendez_Pablo_Ejercicio3.py
@@ -8,21 +8,20 @@ class tRNA(class1.gene):
             length: int = 0, 
             seq: str = '', 
             gc_con: float = 0.0, 
-            organism: str = '', 
             lecture_frame: int = 1, 
             anticodon: str = 'CAU',
             loops: bool = True,
             discrim_base: str = 'A',
             acceptor_stem: bool = True
     ):
-        super().__init__(name, length, seq, gc_con, organism, lecture_frame)
+        super().__init__(name, length, seq, gc_con, lecture_frame)
         self.anticodon = anticodon.upper().replace('T', 'U')
         self.loops = loops
         self.discrim_base = discrim_base
         self.acceptor_stem = acceptor_stem
         self.seq = seq.upper().replace('T', 'U')
     
-    def paired_codon(self):
+    def paired_codon(self) -> str:
         comp = {
             'A' : 'U',
             'U' : 'A',
@@ -38,8 +37,10 @@ class tRNA(class1.gene):
     def codon_translator(self) -> str:
         if (l := len(self.anticodon)) > 3 or l < 3:
             raise ValueError(f"\n[ERROR] Anticodon must have 3 bases length, it has {l}\n")
+        
         if self.noncanonical_bases():
             raise ValueError(f'\n[ERROR] The anticodon sequence {self.anticodon} contains noncanonical bases\n')
+        
         comp_codon = self.paired_codon()
         codon_aa = {
             'Met': {'AUG'},
@@ -64,8 +65,45 @@ class tRNA(class1.gene):
             'Gly': {'GGU', 'GGC', 'GGA', 'GGG'},
             'STOP': {'UAA', 'UAG', 'UGA'}
         }
-        for aa in codon_aa:
-            if comp_codon in codon_aa[aa]:
+        for aa, codons in codon_aa.items():
+            if comp_codon in codons:
                 return aa
             
         raise ValueError(f"\n[ERROR] No aminoacid could match {self.anticodon} anticodon\n")
+    
+class ncRNA():
+    def __init__(
+            self, 
+            seq: str = '', 
+            gc_con: float = 0, 
+            sec_struct: str = 'hairpin', 
+            celular_locat: str = 'cytosol',
+            seed_seq: str = None,
+            complex: str = '',
+            circular: bool = False
+    ):
+        self.length = len(seq)
+        self.gc_con = gc_con
+        self.seq = seq.upper().replace('T', 'U')
+        self.second_struct = sec_struct.lower()
+        self.celular_locat = celular_locat.lower()
+        self.circular = circular
+        self.seed_seq = seed_seq
+        self.complex = complex
+
+    def ncRNA_type(self) -> str:
+        if self.circular:
+            return 'circRNA'
+        elif self.length > 200:
+            return 'lncRNA'
+        elif self.seed_seq:
+            return 'miRNA'
+        elif 'PIWI' in self.complex:
+            return 'piRNA'
+        else:
+            raise ValueError(f"\n[WARNING] No match for the given ncRNA\n")
+        
+    def seed_sequence(self, ncrna_type: str = ''):
+        if ncrna_type != 'miRNA':
+            return None
+        return self.seq[1:9]
