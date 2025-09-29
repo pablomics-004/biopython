8a911b6d9ac82f0da2bc5c5cb59e63f3b6b40594
Pablo Salazar Méndez <pablosm@lcg.unam.mx>
2025-09-28 21:36:23 -0600

    Finalizing script

--- src/SalazarMendez_Pablo_Ejercicio5.py
+++ src/SalazarMendez_Pablo_Ejercicio5.py
@@ -1,7 +1,6 @@
 from Bio.Seq import Seq
 from Bio.SeqUtils import nt_search
 import argparse as ap
-import gc
 
 def my_parser() -> ap.Namespace:
     '''
@@ -81,19 +80,17 @@ def finding_orfs(nt_seq: str, all_frames: bool = False) -> dict[int, list[Seq]]:
     if all_frames:
         dcomp = str(dna_comp := dna_seq.reverse_complement())
 
-    start_codon = 'ATG'
-    stop_codons = {'TAA', 'TAG', 'TGA'}
-    orfs_seq = {}
+    start_codon = 'ATG'; stop_codons = {'TAA', 'TAG', 'TGA'}; orfs_seq = {}; L = len(dseq)
 
-    for i in range(3): # Reading frames
+    # Reading frames
+    for i in range(3):
         orfs_seq.setdefault(i+1, [])
         valid_start = [pos for pos in nt_search(dseq,start_codon)[1:] if pos%3 == i]
-        L = len(dseq)
         for start_idx in valid_start:
             cand_stops = (pos for pos in range(start_idx+3, L-2, 3) if dseq[pos:pos+3] in stop_codons)
             stop_idx = next(cand_stops, None)
-            orf_end_idx = (stop_idx+2) if stop_idx is not None else (L - ((L - start_idx)%3))
-            orfs_seq[i+1].append(dna_seq[start_idx:orf_end_idx].translate(to_stop=True))
+            orf_end_excl = (stop_idx+3) if stop_idx is not None else (L - ((L - start_idx)%3))
+            orfs_seq[i+1].append(dna_seq[start_idx:orf_end_excl].translate(to_stop=True))
 
         if all_frames:
             orfs_seq.setdefault(-(i+1), [])
@@ -101,15 +98,12 @@ def finding_orfs(nt_seq: str, all_frames: bool = False) -> dict[int, list[Seq]]:
             for start_idx in rvalid_start:
                 cand_stops = (pos for pos in range(start_idx+3, L-2, 3) if dcomp[pos:pos+3] in stop_codons)
                 stop_idx = next(cand_stops, None)
-                orf_end_idx = (stop_idx+2) if stop_idx is not None else (L - ((L - start_idx)%3))
-                orfs_seq[-(i+1)].append(dna_comp[start_idx:orf_end_idx].translate(to_stop=True))
-
-    del start_codon, stop_codons, i, start_idx, valid_start, cand_stops, rvalid_start, stop_idx, orf_end_idx
-    gc.collect()
+                orf_end_excl = (stop_idx+3) if stop_idx is not None else (L - ((L - start_idx)%3))
+                orfs_seq[-(i+1)].append(dna_comp[start_idx:orf_end_excl].translate(to_stop=True))
 
     return orfs_seq
 
-def greatest_chain(orfs: dict[int, list[Seq]]) -> str:
+def greatest_chain(orfs: dict[int, list[Seq]]) -> list[str]:
     '''
     Given a dictionary of ORF-aminoacid sequences this function finds the largest one.
 
@@ -120,19 +114,31 @@ def greatest_chain(orfs: dict[int, list[Seq]]) -> str:
 
     Returns
     -------
-    largest_orf : str
+    best : list[str]
     '''
+    best = []; max_len = 0
 
-    
+    for v in orfs.values():
+        for e in v:
+            actual_len = len(e:=str(e))
+            if actual_len > max_len:
+                max_len = actual_len
+                best = [e]
+            elif actual_len == max_len:
+                best.append(e)
+
+    return best
 
 def main():
     args = my_parser()
     orfs_seq = finding_orfs(args.sequence, args.all_frames)
     largest_orf = greatest_chain(orfs_seq)
     print(
-        f'\nThe largest ORF found in {args.sequence} '
-        f'was {largest_orf}\n'
+        f'\nThe largest(s) ORF(s) found in {args.sequence} '
+        'was(were):\n'
     )
+    for e in largest_orf:
+        print(e)
 
 if __name__ == '__main__':
     main()
